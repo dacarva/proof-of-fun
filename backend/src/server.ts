5f0ec19fcd659db1f77ae38f9992124416a9f11a
David Carvajal <davidcarvajal@Davids-MacBook-Pro.local>
2024-02-16 22:53:22 -0500

    Uploading done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,91 +1,66 @@
-/**
- * Setup express server.
- */
-
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
-import path from 'path';
-import helmet from 'helmet';
-import express, { Request, Response, NextFunction } from 'express';
-import logger from 'jet-logger';
-
-import 'express-async-errors';
-
-import BaseRouter from '@src/routes/api';
-import Paths from '@src/constants/Paths';
-
-import EnvVars from '@src/constants/EnvVars';
-import HttpStatusCodes from '@src/constants/HttpStatusCodes';
-
-import { NodeEnvs } from '@src/constants/misc';
-import { RouteError } from '@src/other/classes';
-
-
-// **** Variables **** //
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import express from 'express';
+import busboy from 'busboy';
+import cors from 'cors';
+import { getCircuitInputs } from '../src/util/circuit';
 
 const app = express();
 
+app.use(cors());
 
-// **** Setup **** //
-
-// Basic middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(cookieParser(EnvVars.CookieProps.Secret));
-
-// Show routes called in console during development
-if (EnvVars.NodeEnv === NodeEnvs.Dev.valueOf()) {
-  app.use(morgan('dev'));
-}
-
-// Security
-if (EnvVars.NodeEnv === NodeEnvs.Production.valueOf()) {
-  app.use(helmet());
-}
-
-// Add APIs, must be after middleware
-app.use(Paths.Base, BaseRouter);
-
-// Add error handler
-app.use((
-  err: Error,
-  _: Request,
-  res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  next: NextFunction,
-) => {
-  if (EnvVars.NodeEnv !== NodeEnvs.Test.valueOf()) {
-    logger.err(err, true);
-  }
-  let status = HttpStatusCodes.BAD_REQUEST;
-  if (err instanceof RouteError) {
-    status = err.status;
-  }
-  return res.status(status).json({ error: err.message });
+// Define a route for the root path ("/")
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
+app.post('/upload', (req, res) => {
+  const bb = busboy({ headers: req.headers });
+  let responseSent = false; // Flag to track response status
+  let circuitInputs: any = null;
+  let entireFile: Buffer;
+  let ethAddress: string;
 
-// ** Front-End Content ** //
+  bb.on('file', async (name, file, info) => {
+    const buffers: Buffer[] = [];
+    file.on('data', (data) => {
+      buffers.push(data);
+    });
+    file.on('end', async () => {
+      if (name === 'emailFile') {
+        entireFile = Buffer.concat(buffers);
+      }
+    });
+  });
 
-// Set views directory (html)
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
+  bb.on('field', (name, val, info) => {
+    if (name === 'userInfo') {
+      const userInfo = JSON.parse(val);
+      ethAddress = userInfo.address;
+    }
+  });
 
-// Set static directory (js and css).
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
-
-// Nav to users pg by default
-app.get('/', (_: Request, res: Response) => {
-  return res.redirect('/users');
+  bb.on('finish', async () => {
+    if (!responseSent) {
+      const circuitInputs = await getCircuitInputs(entireFile, ethAddress);
+      res.send('File uploaded and processed successfully.');
+      responseSent = true;
+    }
+  });
+  bb.on('error', (err) => {
+    if (!responseSent) {
+      console.error('File upload error:', err);
+      res.status(500).send(`File upload failed with error: ${err}`);
+      responseSent = true;
+    }
+  });
+  req.pipe(bb);
 });
 
-// Redirect to login if not logged in.
-app.get('/users', (_: Request, res: Response) => {
-  return res.sendFile('users.html', { root: viewsDir });
+// Start the server and listen on port 3001
+app.listen(3001, () => {
+  console.log('Server listening on port 3001');
 });
-
-
-// **** Export default **** //
 
 export default app;
---
+++ backend/src/util/circuit.ts
@@ -0,0 +1,59 @@
+import { verifyDKIMSignature } from '@zk-email/helpers/dist/dkim';
+import { generateCircuitInputs, sha256Pad } from '@zk-email/helpers/';
+import { bytesToBigInt, fromHex, findSelector } from './misc';
+
+export const getCircuitInputs = async (file: Buffer, ethAddress: string) => {
+  // Do something with the file
+  const MAX_BODY_PADDED_BYTES = 512;
+  const STRING_PRESELECTOR = 'Your credit score is: ';
+  try {
+    const dkimResult = await verifyDKIMSignature(file);
+    const circuitInputs = {
+      ...generateCircuitInputs({
+        rsaSignature: dkimResult.signature,
+        rsaPublicKey: dkimResult.publicKey,
+        body: dkimResult.body,
+        bodyHash: dkimResult.bodyHash,
+        message: dkimResult.message,
+        maxMessageLength: 576,
+        maxBodyLength: 512,
+        shaPrecomputeSelector: STRING_PRESELECTOR,
+      }),
+    };
+
+    const address = bytesToBigInt(fromHex(ethAddress)).toString();
+    //@ts-ignore
+    circuitInputs.address = address;
+
+    const selector = STRING_PRESELECTOR.split('').map((char) =>
+      char.charCodeAt(0)
+    );
+    const calc_length =
+      Math.floor((dkimResult.body.length + 63 + 65) / 64) * 64;
+    const [bodyPadded] = await sha256Pad(
+      dkimResult.body,
+      Math.max(MAX_BODY_PADDED_BYTES, calc_length)
+    );
+
+    const shaCutoffIndex =
+      Math.floor((await findSelector(bodyPadded, selector)) / 64) * 64;
+    const bodyRemaining = bodyPadded.slice(shaCutoffIndex);
+    const CREDIT_SCORE_SELECTOR = Buffer.from(STRING_PRESELECTOR);
+    const credit_score_idx = (
+      Buffer.from(bodyRemaining).indexOf(CREDIT_SCORE_SELECTOR) +
+      CREDIT_SCORE_SELECTOR.length
+    ).toString();
+    //@ts-ignore
+    circuitInputs.credit_score_idx = credit_score_idx;
+    console.log('In_padded length', circuitInputs.in_padded.length);
+    console.log('Message length', dkimResult.message.length);
+    console.log('Body length', dkimResult.body.length);
+    console.log('In_body_padded length', circuitInputs.in_body_padded?.length);
+    if (!circuitInputs.in_body_padded?.length) throw new Error('No body');
+
+    return circuitInputs;
+  } catch (error) {
+    console.error('Error processing file:', error);
+    return null;
+  }
+};
